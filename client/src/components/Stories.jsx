c74b23fd3f8cc8f42fbf2637d33875938914381c
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-18 00:15:58 -0500

    creaed Stories.jsx

--- client/src/components/Stories.jsx
+++ client/src/components/Stories.jsx
@@ -1,5 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/free-mode";
 
 const Stories = () => {
   const { currentUser } = useContext(AuthContext);
@@ -26,22 +31,53 @@ const Stories = () => {
       name: "John Doe",
       img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     },
+    {
+      id: 5,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 6,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 7,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 8,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
   ];
 
   return (
-    <div className="stories">
-      <div className="story">
-        <img src={currentUser.profilePic} alt="" />
-        <span>{currentUser.name}</span>
-        <button>+</button>
-      </div>
-      {stories.map((story) => (
-        <div className="story" key={story.id}>
-          <img src={story.img} alt="" />
-          <span>{story.name}</span>
+    <Swiper
+      slidesPerView={5}
+      spaceBetween={40}
+      freeMode={true}
+      modules={FreeMode}
+      className="stories"
+      style={{ width: "100%", height: "100%" }}
+    >
+      <SwiperSlide>
+        <div className="story">
+          <img src={currentUser.profilePic} alt="" />
+          <span>{currentUser.name}</span>
+          <button>+</button>
         </div>
+      </SwiperSlide>
+      {stories.map((story) => (
+        <SwiperSlide>
+          <div className="story" key={story.id}>
+            <img src={story.img} alt="" />
+            <span>{story.name}</span>
+          </div>
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 };
 
